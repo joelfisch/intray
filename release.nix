852af204e197c7d82ce5c2af3ecc34fbdb93f011
Tom Sydney Kerckhove <syd@cs-syd.eu>
2018-07-24 15:22:04 +0200

    Hydra test

---
+++ release.nix
@@ -0,0 +1,16 @@
+let pkgs = import ./default.nix;
+in {
+  packages =
+    [
+      pkgs.haskellPackages.intray-api
+      pkgs.haskellPackages.intray-api-gen
+      pkgs.haskellPackages.intray-cli
+      pkgs.haskellPackages.intray-client
+      pkgs.haskellPackages.intray-client-gen
+      pkgs.haskellPackages.intray-data
+      pkgs.haskellPackages.intray-data-gen
+      pkgs.haskellPackages.intray-server
+      pkgs.haskellPackages.intray-server-test-utils
+      pkgs.haskellPackages.intray-web-server
+    ]
+};
