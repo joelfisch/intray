93f81e31d7bbed90b2ff95b9f343dcce029e0579
Tom Sydney Kerckhove <syd@cs-syd.eu>
2018-07-24 15:29:24 +0200

    try again

--- release.nix
+++ release.nix
@@ -1,16 +1,13 @@
 let pkgs = import ./default.nix;
 in {
-  packages =
-    [
-      pkgs.haskellPackages.intray-api
-      pkgs.haskellPackages.intray-api-gen
-      pkgs.haskellPackages.intray-cli
-      pkgs.haskellPackages.intray-client
-      pkgs.haskellPackages.intray-client-gen
-      pkgs.haskellPackages.intray-data
-      pkgs.haskellPackages.intray-data-gen
-      pkgs.haskellPackages.intray-server
-      pkgs.haskellPackages.intray-server-test-utils
-      pkgs.haskellPackages.intray-web-server
-    ];
+ intray-api               =    pkgs.haskellPackages.intray-api;
+ intray-api-gen           =    pkgs.haskellPackages.intray-api-gen;
+ intray-cli               =    pkgs.haskellPackages.intray-cli;
+ intray-client            =    pkgs.haskellPackages.intray-client;
+ intray-client-gen        =    pkgs.haskellPackages.intray-client-gen;
+ intray-data              =    pkgs.haskellPackages.intray-data;
+ intray-data-gen          =    pkgs.haskellPackages.intray-data-gen;
+ intray-server            =    pkgs.haskellPackages.intray-server;
+ intray-server-test-utils =    pkgs.haskellPackages.intray-server-test-utils;
+ intray-web-server        =    pkgs.haskellPackages.intray-web-server;
 }
