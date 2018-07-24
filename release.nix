888a4f35408abfcd6f2a209b7a3289c540b8cb36
Tom Sydney Kerckhove <syd@cs-syd.eu>
2018-07-24 15:39:43 +0200

    woops

--- release.nix
+++ release.nix
@@ -4,7 +4,6 @@ in {
  intray-api-gen           =    pkgs.haskellPackages.intray-api-gen;
  intray-cli               =    pkgs.haskellPackages.intray-cli;
  intray-client            =    pkgs.haskellPackages.intray-client;
- intray-client-gen        =    pkgs.haskellPackages.intray-client-gen;
  intray-data              =    pkgs.haskellPackages.intray-data;
  intray-data-gen          =    pkgs.haskellPackages.intray-data-gen;
  intray-server            =    pkgs.haskellPackages.intray-server;
