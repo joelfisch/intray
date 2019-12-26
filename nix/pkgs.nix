cbbf4e785e52e763a669c223ee23ca1c58f9a266
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-12-26 16:07:20 +0100

    refactored the nix build

---
+++ nix/pkgs.nix
@@ -0,0 +1,12 @@
+let
+  pkgsv = import (import ./nixpkgs.nix);
+  pkgs = pkgsv {};
+  validity-overlay = import (pkgs.fetchFromGitHub (import ./validity-version.nix) + "/nix/overlay.nix");
+in pkgsv {
+  overlays = [
+    validity-overlay
+    ( import ./gitignore-src.nix )
+    ( import ./overlay.nix )
+  ];
+  config.allowUnfree = true;
+}
