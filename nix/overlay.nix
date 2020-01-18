6b5c0eaede6924db53d67afff54370199a6f46d4
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-01-18 14:32:06 +0100

    improved the way nix overlays are used

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -76,76 +76,81 @@ with final.haskell.lib;
         });
       };
   haskellPackages =
-    previous.haskellPackages.extend (
-      self: super:
-        let
-          typedUuidRepo =
-            final.fetchFromGitHub {
-              owner = "NorfairKing";
-              repo = "typed-uuid";
-              rev = "4c5739c5e231b1cee6bd568ec55734116691ac8f";
-              sha256 =
-                "sha256:185ki38vyvq5889vqdsw53dcdwssdyl4rzvxfhh6kbby17x2f835";
-            };
-          mergelessRepo =
-            final.fetchFromGitHub {
-              owner = "NorfairKing";
-              repo = "mergeless";
-              rev = "3d5f4b54cc2c4c8c6f33a716bc6b67f376b8d1d5";
-              sha256 =
-                "sha256:0far86wdprvyk8i50y4i5wzc0vcqj5ksdf90jnyyalrbklgxxgkv";
-            };
-          prettyRelativeTimeRepo =
-            final.fetchFromGitHub {
-              owner = "NorfairKing";
-              repo = "pretty-relative-time";
-              rev = "2abdb2ba83ad47b8369e2ef618c7c21b82d80b23";
-              sha256 =
-                "sha256:13csx5p0y7wsf8w1vzi3h3bnm4s5976rw9r9glhi55xdwq9s65q7";
-            };
-          stripeHaskellRepo =
-            final.fetchFromGitHub {
-              owner = "NorfairKing";
-              repo = "stripe";
-              rev = "7ced8cef1e932d3fb222dfb3c79c25595cdc82ab";
-              sha256 =
-                "sha256:04dsfx568hmmrr7zg5gbqwipdiy7lvpckfk2ayln6gh6zf9jxl13";
-            };
-          yesodStaticRemoteRepo =
-            final.fetchFromGitHub {
-              owner = "NorfairKing";
-              repo = "yesod-static-remote";
-              rev = "22c0a92c1d62f1b8d432003844ef0636a9131b08";
-              sha256 =
-                "sha256:1mz1fb421wccx7mbpn9qaj214w4sl4qali5rclx9fqp685jkfj05";
-            };
-          looperRepo =
-            final.fetchFromGitHub {
-              owner = "NorfairKing";
-              repo = "looper";
-              rev = "929a8ad6a99a84624767bd9d619cc5318c6bda56";
-              sha256 = "07wc2as7p2pz08a9qfx2jm3kz1cvfg73d872il3zhyplbd6yhzbx";
-            };
-          typedUuidPkg =
-            name:
-              super.callCabal2nix name ( typedUuidRepo + "/${name}" ) {};
-          validityPkg =
-            name:
-              super.callCabal2nix name ( validityRepo + "/${name}" ) {};
-          mergelessPkg =
-            name:
-              super.callCabal2nix name ( mergelessRepo + "/${name}" ) {};
-          stripeHaskellPkg =
-            name:
-              dontCheck (
-                super.callCabal2nix name ( stripeHaskellRepo + "/${name}" ) {}
-              );
-        in
-          {
-            pretty-relative-time = super.callCabal2nix "pretty-relative-time" prettyRelativeTimeRepo {};
-            yesod-static-remote = dontCheck (super.callCabal2nix "yesod-static-remote" yesodStaticRemoteRepo {});
+    previous.haskellPackages.override (
+      old:
+        {
+          overrides =
+            final.lib.composeExtensions ( old.overrides or (_: _: {}) ) (
+              self: super:
+                let
+                  typedUuidRepo =
+                    final.fetchFromGitHub {
+                      owner = "NorfairKing";
+                      repo = "typed-uuid";
+                      rev = "4c5739c5e231b1cee6bd568ec55734116691ac8f";
+                      sha256 =
+                        "sha256:185ki38vyvq5889vqdsw53dcdwssdyl4rzvxfhh6kbby17x2f835";
+                    };
+                  mergelessRepo =
+                    final.fetchFromGitHub {
+                      owner = "NorfairKing";
+                      repo = "mergeless";
+                      rev = "3d5f4b54cc2c4c8c6f33a716bc6b67f376b8d1d5";
+                      sha256 =
+                        "sha256:0far86wdprvyk8i50y4i5wzc0vcqj5ksdf90jnyyalrbklgxxgkv";
+                    };
+                  prettyRelativeTimeRepo =
+                    final.fetchFromGitHub {
+                      owner = "NorfairKing";
+                      repo = "pretty-relative-time";
+                      rev = "2abdb2ba83ad47b8369e2ef618c7c21b82d80b23";
+                      sha256 =
+                        "sha256:13csx5p0y7wsf8w1vzi3h3bnm4s5976rw9r9glhi55xdwq9s65q7";
+                    };
+                  stripeHaskellRepo =
+                    final.fetchFromGitHub {
+                      owner = "NorfairKing";
+                      repo = "stripe";
+                      rev = "7ced8cef1e932d3fb222dfb3c79c25595cdc82ab";
+                      sha256 =
+                        "sha256:04dsfx568hmmrr7zg5gbqwipdiy7lvpckfk2ayln6gh6zf9jxl13";
+                    };
+                  yesodStaticRemoteRepo =
+                    final.fetchFromGitHub {
+                      owner = "NorfairKing";
+                      repo = "yesod-static-remote";
+                      rev = "22c0a92c1d62f1b8d432003844ef0636a9131b08";
+                      sha256 =
+                        "sha256:1mz1fb421wccx7mbpn9qaj214w4sl4qali5rclx9fqp685jkfj05";
+                    };
+                  looperRepo =
+                    final.fetchFromGitHub {
+                      owner = "NorfairKing";
+                      repo = "looper";
+                      rev = "929a8ad6a99a84624767bd9d619cc5318c6bda56";
+                      sha256 =
+                        "07wc2as7p2pz08a9qfx2jm3kz1cvfg73d872il3zhyplbd6yhzbx";
+                    };
+                  typedUuidPkg =
+                    name:
+                      self.callCabal2nix name ( typedUuidRepo + "/${name}" ) {};
+                  validityPkg =
+                    name:
+                      self.callCabal2nix name ( validityRepo + "/${name}" ) {};
+                  mergelessPkg =
+                    name:
+                      self.callCabal2nix name ( mergelessRepo + "/${name}" ) {};
+                  stripeHaskellPkg =
+                    name:
+                      dontCheck (
+                        self.callCabal2nix name ( stripeHaskellRepo + "/${name}" ) {}
+                      );
+                in
+                  {
+            pretty-relative-time = self.callCabal2nix "pretty-relative-time" prettyRelativeTimeRepo {};
+            yesod-static-remote = dontCheck (self.callCabal2nix "yesod-static-remote" yesodStaticRemoteRepo {});
             servant-auth-server = doJailbreak (super.servant-auth-server);
-            looper = super.callCabal2nix "looper" looperRepo {};
+            looper = self.callCabal2nix "looper" looperRepo {};
 
           } // final.lib.genAttrs [
             "stripe-core"
@@ -159,5 +164,7 @@ with final.haskell.lib;
             "mergeless"
             "genvalidity-mergeless"
           ] mergelessPkg // final.intrayPackages
+            );
+        }
     );
 }
