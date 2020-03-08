7efd301ffe08e6db526d9460b03e70508e3f9e80
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-03-08 23:38:19 +0100

    failing test case for registration without url

--- intray-cli/test/Intray/Cli/NoSyncSpec.hs
+++ intray-cli/test/Intray/Cli/NoSyncSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Intray.Cli.NoSyncSpec
   ( spec
@@ -8,9 +9,44 @@ import TestImport
 
 import Intray.Cli
 import Intray.Cli.OptParse
+import Intray.Data
 
 spec :: Spec
 spec = do
+  it "correctly errors when a user tries to register but has no server configured" $
+    withSystemTempDir "intray-cli-test-data" $ \dataDir ->
+      withSystemTempDir "intray-cli-test-cache" $ \cacheDir -> do
+        let sets =
+              Settings
+                { setBaseUrl = Nothing
+                , setUsername = Nothing
+                , setCacheDir = cacheDir
+                , setDataDir = dataDir
+                , setSyncStrategy = NeverSync
+                }
+        let intray d = runReaderT (dispatch d) sets
+        let rs =
+              RegisterSettings
+                { registerSetUsername = parseUsername "testuser"
+                , registerSetPassword = Just "password"
+                }
+        intray (DispatchRegister rs) `shouldThrow` (\(_ :: ExitCode) -> True)
+  it "correctly errors when a user tries to login but has no server configured" $
+    withSystemTempDir "intray-cli-test-data" $ \dataDir ->
+      withSystemTempDir "intray-cli-test-cache" $ \cacheDir -> do
+        let sets =
+              Settings
+                { setBaseUrl = Nothing
+                , setUsername = Nothing
+                , setCacheDir = cacheDir
+                , setDataDir = dataDir
+                , setSyncStrategy = NeverSync
+                }
+        let intray d = runReaderT (dispatch d) sets
+        let rs =
+              LoginSettings
+                {loginSetUsername = parseUsername "testuser", loginSetPassword = Just "password"}
+        intray (DispatchLogin rs) `shouldThrow` (\(_ :: ExitCode) -> True)
   it "Works fine without a server" $
     withSystemTempDir "intray-cli-test-cache" $ \cacheDir ->
       withSystemTempDir "intray-cli-test-data" $ \dataDir -> do
