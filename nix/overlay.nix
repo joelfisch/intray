226895923437625f543a16445b422f224c650b28
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-04-08 17:12:04 +0200

    reformatted nix code

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -11,56 +11,82 @@ with final.haskell.lib;
             failOnAllWarnings (
               disableLibraryProfiling ( final.haskellPackages.callCabal2nix name ( pathFor name ) {} )
             )
-            ) ( final.haskellPackages.autoexporter );
+          ) ( final.haskellPackages.autoexporter );
       intrayPkgWithComp =
         exeName: name:
           generateOptparseApplicativeCompletion exeName ( intrayPkg name );
       intrayPkgWithOwnComp = name: intrayPkgWithComp name name;
     in {
 
-        "intray-api" = intrayPkg "intray-api";
-        "intray-api-gen" = intrayPkg "intray-api-gen";
-        "intray-cli" = intrayPkgWithComp "intray" "intray-cli";
-        "intray-client" = intrayPkg "intray-client";
-        "intray-data" = intrayPkg "intray-data";
-        "intray-data-gen" = intrayPkg "intray-data-gen";
-        "intray-server" = intrayPkgWithOwnComp "intray-server";
-        "intray-server-gen" = intrayPkg "intray-server-gen";
-        "intray-web-server" =
-        let semantic-js = builtins.fetchurl {
-              url = https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js;
-              sha256 = "sha256:0ll00jawcwd4nj568sj7lfp2ixrni9wqf37sz5nhz6wggjk9xhdp";
+      "intray-api" = intrayPkg "intray-api";
+      "intray-api-gen" = intrayPkg "intray-api-gen";
+      "intray-cli" = intrayPkgWithComp "intray" "intray-cli";
+      "intray-client" = intrayPkg "intray-client";
+      "intray-data" = intrayPkg "intray-data";
+      "intray-data-gen" = intrayPkg "intray-data-gen";
+      "intray-server" = intrayPkgWithOwnComp "intray-server";
+      "intray-server-gen" = intrayPkg "intray-server-gen";
+      "intray-web-server" =
+        let
+          semantic-js =
+            builtins.fetchurl {
+              url =
+                https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js;
+              sha256 =
+                "sha256:0ll00jawcwd4nj568sj7lfp2ixrni9wqf37sz5nhz6wggjk9xhdp";
             };
-            semantic-css = builtins.fetchurl {
-              url = https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css;
-              sha256 = "sha256:0m13jdkv3vdqr0pbr1zfc2ndsafr2p5mnfzkbm7pd8v1ylwy8rpn";
+          semantic-css =
+            builtins.fetchurl {
+              url =
+                https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css;
+              sha256 =
+                "sha256:0m13jdkv3vdqr0pbr1zfc2ndsafr2p5mnfzkbm7pd8v1ylwy8rpn";
             };
-            jquery-js = builtins.fetchurl {
+          jquery-js =
+            builtins.fetchurl {
               url = https://code.jquery.com/jquery-3.1.1.min.js;
-              sha256 = "sha256:1gyrxy9219l11mn8c6538hnh3gr6idmimm7wv37183c0m1hnfmc5";
+              sha256 =
+                "sha256:1gyrxy9219l11mn8c6538hnh3gr6idmimm7wv37183c0m1hnfmc5";
             };
-            icons-ttf = builtins.fetchurl {
-              url = https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/themes/default/assets/fonts/icons.ttf;
-              sha256 = "sha256:1nm34hrh3inyrq7cbkh47g8m2hbqpsgkzbdrpfiiii7m8bsq2zyb";
+          icons-ttf =
+            builtins.fetchurl {
+              url =
+                https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/themes/default/assets/fonts/icons.ttf;
+              sha256 =
+                "sha256:1nm34hrh3inyrq7cbkh47g8m2hbqpsgkzbdrpfiiii7m8bsq2zyb";
             };
-            icons-woff = builtins.fetchurl {
-              url = https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/themes/default/assets/fonts/icons.woff;
-              sha256 = "sha256:1qgzlmd80c4ckh9zpfl2qzjvg389hvmkdhkv8amyq4c71y2a9dlm";
+          icons-woff =
+            builtins.fetchurl {
+              url =
+                https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/themes/default/assets/fonts/icons.woff;
+              sha256 =
+                "sha256:1qgzlmd80c4ckh9zpfl2qzjvg389hvmkdhkv8amyq4c71y2a9dlm";
             };
-            icons-woff2 = builtins.fetchurl {
-              url = https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/themes/default/assets/fonts/icons.woff2;
-              sha256 = "sha256:1lqd60f1pml8zc93hgwcm6amkcy6rnbq3cyxqv5a3a25jnsnci23";
+          icons-woff2 =
+            builtins.fetchurl {
+              url =
+                https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/themes/default/assets/fonts/icons.woff2;
+              sha256 =
+                "sha256:1lqd60f1pml8zc93hgwcm6amkcy6rnbq3cyxqv5a3a25jnsnci23";
             };
-            intrayAndroidRelease = 
-              let
-                repo = final.fetchgit {
-                  url = "https://gitlab.com/Norfair/intray-android-release.git";
+          intrayAndroidRelease =
+            let
+              repo =
+                final.fetchgit {
+                  url =
+                    "https://gitlab.com/Norfair/intray-android-release.git";
                   rev = "68bb95a3cdb3721e41b4820cff92da9e863dd8a7";
-                  sha256 = "sha256:18jxp39ln1jcd00myg928j3m1qr71ls5r3ch1fa4jp72waik4khl";
+                  sha256 =
+                    "sha256:18jxp39ln1jcd00myg928j3m1qr71ls5r3ch1fa4jp72waik4khl";
                 };
-              in repo + "/app-release.apk";
-        in overrideCabal (intrayPkgWithOwnComp "intray-web-server") (old: {
-          preConfigure = ''
+            in
+              repo + "/app-release.apk";
+        in
+          overrideCabal ( intrayPkgWithOwnComp "intray-web-server" ) (
+            old:
+              {
+                preConfigure =
+                  ''
             ${old.preConfigure or ""}
 
             mkdir -p static/
@@ -74,8 +100,9 @@ with final.haskell.lib;
             cp ${icons-woff2} static/semantic/themes/default/assets/fonts/icons.woff2
             cp ${intrayAndroidRelease} static/intray.apk
           '';
-        });
-      };
+              }
+          );
+    };
   haskellPackages =
     previous.haskellPackages.override (
       old:
