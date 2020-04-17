2e1fed3411c85936970c8b1c258d788e1978f94e
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-04-17 19:17:44 +0200

    upgraded mergeless version

--- nix/mergeless-version.nix
+++ nix/mergeless-version.nix
@@ -1,6 +1,6 @@
 {
   owner = "NorfairKing";
   repo = "mergeless";
-  rev = "bf61e179d1935282fd32a85121613d2b98a927d9";
-  sha256 = "sha256:1q0hhy9kbpsazia5hrqayd960yzz7f6s4dp0byjgi7gawpzlpfc5";
+  rev = "32fb75445723213ed78f678c20a2271a48e5dd41";
+  sha256 = "sha256:0zhd1y84j8q1d3acyrf5fhf9l74dkqcx72m5jms6sg317j0ln0m7";
 }
